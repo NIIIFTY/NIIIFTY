04e482dd1561c36db7b30bcab158597411a54f22
Exhibit <exhibitdotso@gmail.com>
2026-05-07 14:02:42 +0100

    fix: rename proxy.ts to middleware.ts

--- src/app/api/auth/route.ts
+++ src/app/api/auth/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-// see proxy.ts
+// see middleware.ts
 export async function GET(request: NextRequest) {
   return new NextResponse('Auth Required.', {
     status: 401,
